6531722f7ab9ab93c1126e5308bb51366c2c2540
ctbrahmstedt <40968636+ctbrahmstedt@users.noreply.github.com>
2022-06-22 14:48:49 -0600

    Update main.js

--- main.js
+++ main.js
@@ -4,13 +4,40 @@ function onInit(){
   pinMode(D30, "input_pullup");
   pinMode(D31, "input_pullup");
 }
+
+function pullups(){
+  pinMode(D29, "input_pullup");
+  pinMode(D30, "input_pullup");
+  pinMode(D31, "input_pullup");
+}
+
+function pulldowns(){
+  pinMode(D29, "input_pulldown");
+  pinMode(D30, "input_pulldown");
+  pinMode(D31, "input_pulldown");
+}
 // Write data
 setInterval(function() {
+  var g = require("Storage").open("log","r");
+  var length=g.len;
+  pullups();
+  if(require("Storage").getFree()>28670){ //28672 is target
   f.write(getTime().toFixed(0)+","+analogRead(D29)+","+analogRead(D30)+","+analogRead(D31)+"\n");
+  } pulldowns();
 }, 60*5*1000); //data logging interval (in milliseconds) set for 5 minutes. 
+
+setInterval(function() {
+  var g = require("Storage").open("log","r");
+  var length=g.len;
+  pullups();
+  if((require("Storage").getFree()<28671)&&(require("Storage").getFree()>5000)){
+  f.write(getTime().toFixed(0)+","+analogRead(D29)+","+analogRead(D30)+","+analogRead(D31)+"\n");
+  } pulldowns();
+}, 60*1*1000); //data logging interval (in milliseconds) set for 5 minutes. 
 
 function getData(callback) {
   var f = require("Storage").open("log","r");
+  var length=f.len;
   var l = f.readLine();
   while (l!==undefined) {
     callback(l);
